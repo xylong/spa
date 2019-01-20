44eff5dc6530dbcaeba79ab6af6e80547b006eb2
xylong <416319808@qq.com>
2019-01-20 18:27:10 +0800

    安装element-ui、测试

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,9 +2,12 @@ require('./bootstrap');
 
 import Vue from 'vue';
 import Vuex from 'vuex';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './pages/App';
 
 Vue.use(Vuex);
+Vue.use(ElementUI);
 
 const app = new Vue({
     el: '#app',
