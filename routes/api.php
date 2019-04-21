d3ea8839c94fefaedb931014b0c421b225ce1bed
xylong <416319808@qq.com>
2019-04-21 10:13:48 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->group([
+        'namespace'=>'App\Api\V1\Controllers',
+        'middleware' => 'api',
+        'prefix' => 'auth',
+    ], function ($api) {
+        $api->post('login', 'AuthController@login');
+        $api->post('logout', 'AuthController@logout');
+        $api->post('refresh', 'AuthController@refresh');
+        $api->post('me', 'AuthController@me');
+    });
 });
